3007453f21fbbcae2281211ab5ea451bbc65aa01
qioixiy <qioixiy@gmail.com>
2015-11-29 18:01:50 +0800

    rename layout_gridview to LayoutGridview

--- src/com/example/bishe/MainActivity.java
+++ src/com/example/bishe/MainActivity.java
@@ -51,33 +51,6 @@ public class MainActivity extends Activity {
 				runHttpsConnection();
 			}
 		});
-
-/*
-		GridView gridview = (GridView) findViewById(R.id.GridView);
-		ArrayList<HashMap<String, Object>> meumList = new ArrayList<HashMap<String, Object>>();
-		for (int i = 1; i < 10; i++) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("ItemImage", R.drawable.main_item);
-			map.put("ItemText", "" + i);
-			meumList.add(map);
-		}
-		SimpleAdapter saItem = new SimpleAdapter(this, meumList, // 数据源
-				R.layout.item, // xml实现
-				new String[] { "ItemImage", "ItemText" }, // 对应map的Key
-				new int[] { R.id.ItemImage, R.id.ItemText }); // 对应R的Id
-
-		// 添加Item到网格中
-		gridview.setAdapter(saItem);
-		// 添加点击事件
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				int index = arg2 + 1;// id是从0开始的，所以需要+1
-				Toast.makeText(getApplicationContext(), "你按下了选项：" + index, 0)
-						.show();
-				// Toast用于向用户显示一些帮助/提示
-			}
-		});*/
 	}
 
 	private void runHttpsConnection() {
