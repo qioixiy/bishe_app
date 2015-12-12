858fa4d64967164579b06706a0500c33fdc8ce3e
qioixiy <qioixiy@gmail.com>
2015-12-12 23:11:53 +0800

    gridview.setOnItemClickListener

--- src/com/example/bishe/MainActivity.java
+++ src/com/example/bishe/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.bishe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
 /**
  * @Description:主页
@@ -25,6 +29,17 @@ public class MainActivity extends Activity {
 	private void initView() {
 		gridview = (MyGridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new MyGridAdapter(this));
+		// 添加点击事件
+		gridview.setOnItemClickListener(new OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				int index = arg2 + 1;
+				Toast.makeText(getApplicationContext(), "你按下了选项：" + index,
+						Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
